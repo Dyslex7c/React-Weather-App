b67354422e205cd7656f0e5f61a2e4b67d8139a3
Dyslex7c <polynsominiac@gmail.com>
2024-07-12 18:07:28 +0530

    rectified

---
+++ src/components/search/api.js
@@ -0,0 +1,15 @@
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '2eaaa97431msh65b8c9974942b59p1d04cejsn177d1954037d',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const REVERSE_API_URL = "https://api.openweathermap.org/geo/1.0"
+
+export const WEATHER_API_KEY = "34752c03213f4be0fc511f916d8d5195";
